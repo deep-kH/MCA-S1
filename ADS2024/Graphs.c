cdf4f8ad423d497bfb585653c4d4624bd61eb34f
Deepak H <deepakdeepam003@gmail.com>
2024-12-12 22:52:12 +0530

    Graphs Using Adjacency List

--- ADS2024/Graphs.c
+++ ADS2024/Graphs.c
@@ -1,75 +1,128 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Graphsnode
+typedef struct Node
 {
     int data;
-    struct Graphsnode *Next;
+    struct Node *next;
 } node;
 
-node *new, *temp, *current;
-void main()
+node *new, *temp;
+int size;
+
+node *createNode(int data)
 {
-    int n, m;
-    printf("Enter number of vertices in your Graph : ");
-    scanf("%d", &n);
+    new = (node *)malloc(sizeof(node));
+    new->data = data;
+    new->next = NULL;
+    return new;
+}
 
-    node *Graph[n];
-
-    for (int i = 0; i < n; i++)
-
-    
+int findIndex(node *V[], int size, int data)
+{
+    for (int i = 0; i < size; i++)
     {
-        new = (node *)malloc(sizeof(node));
-        new->Next = NULL;
-        printf("Enter Value of Vertex : ");
-        scanf("%d", &new->data);
-        Graph[i] = new;
+        if (V[i]->data == data)
+            return i;
+    }
+    return -1;
+}
+
+int addedge(node *V[], int v1, int v2)
+{
+    int j;
+    if (findIndex(V, size, v1) == -1 || findIndex(V, size, v2) == -1)
+    {
+        printf("Entered Vertices are not in your Graph!");
+        j = 0;
+    }
+    if (v1 == v2)
+    {
+        printf("Can't ADD Loops in a simple Graph!");
+        j = 0;
     }
 
-    for (int i = 0; i < n; i++)
+    else
     {
-        temp = Graph[i];
+        new = createNode(v2);
+        int i = findIndex(V, size, v1);
+        if (i != -1)
+        {
+            new->next = V[i]->next;
+            V[i]->next = new;
+        }
+
+        new = createNode(v1);
+        i = findIndex(V, size, v2);
+        if (i != -1)
+        {
+            new->next = V[i]->next;
+            V[i]->next = new;
+        }
+        j = 1;
+    }
+    return j;
+}
+
+void printGraph(node *V[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d - ", V[i]->data);
+        temp = V[i]->next;
         while (temp)
         {
-            if (temp == NULL)
-            {
-                printf("NULL");
-                break;
-            }
-            else
-                printf("%d, ", temp->data);
-            temp = temp->Next;
+            printf("%d , ", temp->data);
+            temp = temp->next;
         }
+        if (temp == NULL)
+            printf("NULL");
         printf("\n");
     }
+}
 
-    // for (int i = 0; i < n; i++)
-    // {
-    //     printf("Enter Neighbour of %d", Graph[i]);
-    //     for (int j = 0; j < n; j++)
-    //     {
-    //         printf("Edge(1/0)");
-    //         scanf("%d", &m);
-    //         if (!m)
-    //             break;
+int factorial(int n)
+{
+    if (n > 0)
+        return n + factorial(n - 1);
+    else
+        return 0;
+}
 
-    //         while (m)
-    //         {
-    //             printf("Enter its Neighbours : ");
-    //             scanf("%d", new->data);
-    //             if (Graph[i]->Next == NULL)
-    //             {
-    //                 Graph[i]->Next = new;
-    //                 current = new;
-    //             }
-    //             else
-    //             {
-    //                 current->Next = new;
-    //                 current = new;
-    //             }
-    //             m = 0;
-    //         }
-    //     }
-    // }
+void main()
+{
+    int v1, v2, falg, hi;
+    printf("Enter Number of Vertex : ");
+    scanf("%d", &size);
+    node *V[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        int data;
+        printf("Enter vertex %d: ", i + 1);
+        scanf("%d", &data);
+        V[i] = createNode(data);
+    }
+
+    for (int i = 0; i < factorial(size - 1); i++)
+    {
+        printf("\n%d\n", i);
+        printf("ADD edge?(1/0) : ");
+        scanf("%d", &falg);
+        if (falg)
+        {
+            printf("Enter Vertex 1 for the edge : ");
+            scanf("%d", &v1);
+            printf("Enter Vertex 1 for the edge : ");
+            scanf("%d", &v2);
+
+            hi = addedge(V, v1, v2);
+            if (!hi)
+                i--;
+        }
+        else
+            break;
+    }
+
+    printGraph(V, size);
 }
