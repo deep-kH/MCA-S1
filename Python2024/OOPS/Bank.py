7baf36842a58321b2a3974e205dc51d38445b625
Hi <Anonymous@gmail.com>
2025-01-06 09:44:36 +0530

    bank.py

--- Python2024/OOPS/Bank.py
+++ Python2024/OOPS/Bank.py
@@ -1,90 +1,47 @@
-import os
-
-class BankAcc:
-    acc_no = 121000
-    def __init__(self,name, acc_type, balance = 0.0):
-        BankAcc.acc_no += 1
-        self.acc_no = BankAcc.acc_no
-        self.name = name
-        self.acc_type = acc_type
-        self.balance = balance
-    
-    def withdraw(self):
-        amount = float(input("Enter the amount to withdraw: "))
-        if amount <= user.balance:
-            user.balance -= amount
-            print(f"{amount} has been withdrawn from your account")
+class Bank_account:
+    def __init__(self,no,name,Balance=0,typ="SAVINGS"):
+        self.__ACno = no
+        self.__ACname = name
+        self.Balance = Balance
+        self.type = typ
+        self.deposit_limit = 100000
+    def check_Bank_Balance(self):
+        return (self.Balance)
+    def widraw(self, amount):
+        if amount > self.Balance:
+            print("You dont Have enough Balance!")
         else:
-            print("Insufficient balance")
-    
-    def deposit(self):
-        amount = float(input("Enter the amount to deposit: "))
-        user.balance += amount
-        print(f"{amount} has been added to your account")
+            print(amount, "rupees is Widrawn from your Account.")
+            self.Balance -= amount
+    def Deposit(self, amount):
+        if amount > self.deposit_limit:
+            print("You have reached your Limit!")
+        else : 
+            print(amount, "rupees is Deposited into Your Account.")
+            self.Balance += amount 
+    def data_of_account(self):
+        print("Your Balance is : ", self.Balance,"rupees")
+        print("Your account type is :",self.type)
+        print("Your Account name is : ",self.__ACname)
+        print("Your Account Number is : ",self.__ACno)
 
-    def __str__(self):
-        return f"Name: {self.name} \nAccount number: {self.acc_no}\nAccount Type: {self.acc_type}\nBalance: {self.balance}"
+p = input("Enter Account Name : ")
+n = int(input("Enter initial Deposit : "))
+t = input("Enter Account type : ")
 
-if __name__ == "__main__":
-    members = dict()
-    while(True):
-        os.system('cls')
-        print("Welcome to Maal Bank")
-        print("1.Create Account")
-        print("2.Login")
-        print("3.Check members")
-        print("4.Exit")
-        ch = int(input("Enter your choice: "))
-        match(ch):
-            case 1:           # creating acc
-                os.system('cls')
-                name = input("\nEnter your name: ").lower()
-                acc_type = input("\nAccount type(savings / current): ").lower()
-                balance = float(input("\nEnter the initial deposit to your account: "))
-                user = BankAcc(name, acc_type, balance)
-                members[user.name] = user
-                print("Your account has been created: \n")
-                print(user)
-                input()
+AC1 = Bank_account(720091567855,p, n, t)
+while 1:
+    choice=int(input("\n1. Display Account details\n2. Check Bank Balance\n3. Deposit Money\n4. Widraw Money\nChoose : "))
+    if choice == 1:
+        AC1.data_of_account()
+        
+    elif choice == 2:
+        print("Your Account balance is : ",AC1.check_Bank_Balance())
+        
+    elif choice == 3:
+        a=int(input("Enter the amount to be Deposited : "))
+        AC1.Deposit(a)
 
-            case 2:
-                name = input("Enter your name: ").lower()
-                if name in members:
-                    user = members[name]
-                    while(True):
-                        os.system('cls')
-                        print("1.Withdraw")
-                        print("2.Deposit")
-                        print("3.Check balance")
-                        print("4.Logout")
-                        choice = int(input("Enter your choice: "))
-                        match(choice):
-                            case 1:
-                                user.withdraw()
-                            case 2:
-                                user.deposit()
-                            case 3:
-                                print(user)
-                            case 4:
-                                break
-                            case _:
-                                print("Invalid Choice")
-                        input()
-                else:
-                    print("user not found")
-
-            case 3:
-                if members:
-                    print("The members are: ")
-                    for member in members.keys():
-                        print(member, end=", ")
-                else:
-                    print("There are no members to display")
-                input()
-
-            case 4:
-                break
-
-            case _:
-                print("Invalid Choice")
-                input()
+    else:
+        b=int(input("Enter the amount to be Widrawn : "))
+        AC1.widraw(b)
